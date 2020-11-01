3ca016a86c96d2f6bff33af81894e74d556d7484
0xdeef <71462612+0xdeef@users.noreply.github.com>
2020-11-01 13:09:31 +0100

    Add files via upload

---
+++ Opdracht 1.py
@@ -0,0 +1,44 @@
+lijstCijfers = [] #Dit is de lijst waar alle cijfers in staan. Deze is momenteel nog leeg, maar deze wordt gevuld door de gebruiker zodra ze cijfers opgeven voor desbetreffende vak.
+start = True # Dit zorgt ervoor dat de while loop kan beginnen omdat start gelijk aan True moet zijn anders start die niet. 
+vraagMinimaal = False # Dit zorgt ervoor dat de while loop voor het uitrekenen alleen pas begint wanneer de gebruiker aangeeft dat hij of zij dit wilt berekenen.
+minimalAanwezig = False # Dit zorgt ervoor wanneer de berekening heeft plaatsgevonden dat deze ook word geprint in het document. 
+
+vraagVak = input("Voor welk vak wil je een cijfer invoeren? : ") # Hier vraag je de gebruiker voor welk vak ze het gemiddelde willen berekenen.
+
+while start == True: # Zolang start gelijk is aan True (start == True) gaat die alles wat hieronder staat herhalen totdat start =/= True oftewel start == False
+    vraagCijfer = float(input(f"Welk cijfer heb je voor {vraagVak} behaald? : ")) # Hier vraag je de gebruiker welk cijfer ze voor dit vak hebben behaald. Omdat er float van tevoren staat zorgt dit ervoor dat het ingevoerde getal een comma getal kan zijn.
+    if vraagCijfer < 1 : # Dit kijkt als het cijfer onder een 1.
+        print("Cijfer moet minmaal een 1 zijn, probeer opnieuw") # Als bovenstaande uitspraak klopt (vraagCijfer < 1) print dan "ijfer moet minmaal een 1 zijn, probeer opnieuw"
+    elif vraagCijfer > 10: # Dit kijkt als het cijfer boven een 10.
+        print("Cijfer moet maximaal een 10 zijn, probeer opnieuw") # Als bovenstaande uitspraak klopt (vraagCijfer > 10) print dan "Cijfer moet lager zijn dan 10, probeer opnieuw"
+    else: # Als bovenstaande uitspraak niet klopt doe dan dit 
+        lijstCijfers.append(vraagCijfer) # Met append die je de ingevoerde cijfers aan de lege lijst toevegen die we bovenaan hebben gedefineerd
+        vraagDoorgaan = input(f"Wil je nog een cijfer invoeren voor {vraagVak} (Ja/Nee) : ").upper() # Hier vragen we als we nog een vak willen invoeren. Met .upper() zorg je ervoor dat alle ingevoerde waarde gelijk is en niet bv. ja, Ja, jA of JA. 
+        if vraagDoorgaan == "NEE": # Als bovenstaande uitspraak gelijk is aan "NEE" doe dan het volgende 
+            start = False # Zet de waarde van start naar False. Dit zorgt ervoor dat de while loop van het begin stopt zodra we klaar zijn met deze loop.
+            minimaalUitrekenen = input(f"Wil je nog invoeren hoeveel cijfers je nog moet ontvangen voor {vraagVak} om minimaal een voldoende te halen (Ja/Nee) : ").upper() # Hier vraag je de gebruiker als hij of zij willen berekenen wat ze minimaal moeten behalen voor de x aantal volgende toetsen.
+            if minimaalUitrekenen == "JA": # Als bovenstaande waarde gelijk is aan "JA" voer dan onderstaande uit
+                vraagMinimaal = True # Zet de waarde voor vraag minimaal op True. Deze word op een later moment gebruikt 
+                vraagAantalMinmaal = int(input(f"Hoeveel cijfer verwacht je nog voor {vraagVak} : ")) # Vraag de gebruiker hoeveel cijfers ze nog verwacht voor het vak
+
+            
+gemiddelde = sum(lijstCijfers) / len(lijstCijfers) # Hier word het gemiddelde berekend 
+bestandsLocatie = input("Waar wil je het document opslaan? : ") #Hier word de gebruiker gevraagd voor de bestandlocatie bv. C:\Users\JohnDoe\Desktop
+bestand = open(f"{bestandsLocatie}\cijfers-{vraagVak}.txt", "a") #Hier word een bestandgeopend op bovenstaande locatie met de naam van het vak.
+
+if  vraagMinimaal == False: # Als de gebruiker heeft gekozen voor niet het minimaal berekenen van aantal cijfers dan doe onderstaande instructies
+    bestand.write(f"Voor {vraagVak} is je gemiddelde {round(gemiddelde, 1)}") #Open het bestand met schrijf rechten en schrijf het vak en wat het gemiddelde is voor dit vak. Rond dit af met 1 cijfer achter de comma.
+
+if vraagMinimaal == True: # Als de gebruiker wel heeft gekozen voor het berekenen van minimaal cijfer voor x komende toetsen voer onderstaande instructies uit
+    aantaalcijfers = len(lijstCijfers) + vraagAantalMinmaal # Voeg het al ingvoerde aantal cijfers + verwachte aantal cijfers bij elkaar op om te weten hoeveel cijfers er intotaal zijn.
+    aantalPunten = aantaalcijfers * 5.5 # Vervolgens het aantal toetsen * 5.5
+    rekenen = aantalPunten - sum(lijstCijfers) # Trek het aantal punten van al bekende cijfers af van het totaal minimaal aantal punten
+    uitkomst = rekenen / vraagAantalMinmaal # Deel de resterende punten door het nog verwachte cijfers 
+    if uitkomst >= 10: # Als het minimale cijfer om een voldoende > 10 doe dan onderstaande instructie
+        print("Helaas! Je moet hoger dan een 10 halen om gemiddeld een voldoende te hebben") # print "Helaas! Je moet hoger dan een 10 halen om gemiddeld een voldoende te hebben"
+    else: # Als vraagMinimaal == True doe dan onderstaande instructie
+            bestand.write(f"Voor {vraagVak} is je gemiddelde {round(gemiddelde, 1)}. Als je de komende {vraagAantalMinmaal} toetsen minimaal {round(uitkomst, 1)} behaald heb je een voldoende!") #Open het bestand met schrijf rechten en schrijf het vak en wat het gemiddelde is voor dit vak. Rond dit af met 1 cijfer achter de comma. En wat de gebruiker minimaal moeten halen voor de X aantal toetsen om minimaal een voldoende te scoren
+
+bestand.close() # Sluit het bestand. 
+    
+print(f"De puntenlijst voor {vraagVak} staat in {bestandsLocatie}") #print waar het bestand is opgeslagen
